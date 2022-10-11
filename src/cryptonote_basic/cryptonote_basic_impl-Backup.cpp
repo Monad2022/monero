5db110a6393f98d3fc680b0e241f6986cb213888
Adam-Osibot <74332472+Adam-Osibot@users.noreply.github.com>
2022-10-11 16:43:48 +1000

    Premine + Tail Emission from start
    - one trillion pre mine block 1
    -tail emission from block 1

--- src/cryptonote_basic/cryptonote_basic_impl-Backup.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl-Backup.cpp
@@ -81,32 +81,16 @@ namespace cryptonote {
   }
   //-----------------------------------------------------------------------------------------------
   bool get_block_reward(size_t median_weight, size_t current_block_weight, uint64_t already_generated_coins, uint64_t &reward, uint8_t version) {
-    
-        //Start Greenero Modification:
-
-    //Add premine for org on block no 1
-    if(already_generated_coins == 0) {
-           base_reward = 1000000000000;
-           return true;
-        }
-
-    //Old Code - May be redundent
     static_assert(DIFFICULTY_TARGET_V2%60==0&&DIFFICULTY_TARGET_V1%60==0,"difficulty targets must be a multiple of 60");
     const int target = version < 2 ? DIFFICULTY_TARGET_V1 : DIFFICULTY_TARGET_V2;
     const int target_minutes = target / 60;
     const int emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE - (target_minutes-1);
 
-    //Old Code - May be redundent - choose between normal and tail emmissions
-    //uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
-    //if (base_reward < FINAL_SUBSIDY_PER_MINUTE*target_minutes)
-    //{
-    //  base_reward = FINAL_SUBSIDY_PER_MINUTE*target_minutes;
-    //}
-
-    //Drop Emmission Speed and Just Use Tail Emmissions
-    uint64_t base_reward = FINAL_SUBSIDY_PER_MINUTE*target_minutes;
-
-    //End Greenero Modification:
+    uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
+    if (base_reward < FINAL_SUBSIDY_PER_MINUTE*target_minutes)
+    {
+      base_reward = FINAL_SUBSIDY_PER_MINUTE*target_minutes;
+    }
 
     uint64_t full_reward_zone = get_min_block_weight(version);
 
